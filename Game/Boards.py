d865ad474e99bd3313001bb9e8b1b3f2eea666e1
Esteban <estebanarivasv@gmail.com>
2019-10-21 21:09:14 -0300

    Practicing with Matrices

---
+++ Game/Boards.py
@@ -0,0 +1,39 @@
+class Boards:
+    def __init__(self, name="", rows=0, columns=0):
+        self.__name = name
+        self.__rows = rows
+        self.__columns = columns
+        self.__board = []
+
+    def setName(self, new_name):
+        self.__name = new_name
+
+    def setRows(self, new_rows):
+        self.__rows = new_rows
+
+    def setColumns(self, new_columns):
+        self.__columns = new_columns
+
+    def getName(self):
+        return self.__name
+
+    def setRows(self):
+        return self.__rows
+
+    def setColumns(self):
+        return self.__columns
+
+    def createBoard(self, rows, columns):
+        for i in range(rows):
+            row = []
+            for j in range(columns):
+                row.append(0)
+            self.__board.append(row)
+
+    def printBoard(self):
+        for i in
+
+b = Boards('Primary', 10, 10)
+b.createBoard(10, 10)
+print(b.printBoard())
+print(b.getName())
---
+++ Game/Main.py
@@ -0,0 +1,5 @@
+'''impor
+principal = Boards("Principal")
+principal.board[[1]for i in range(3)] = 14
+
+print(principal.board)t Boards'''
