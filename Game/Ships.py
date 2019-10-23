8a3ee484e349df39c8b9d6761f641427a2aa86f0
Esteban <estebanarivasv@gmail.com>
2019-10-23 11:08:14 -0300

    Constants created

---
+++ Game/Ships.py
@@ -0,0 +1,13 @@
+"""Constants"""
+
+"""Aircraft Carrier - Portaaviones"""
+a_carrier = ["A", "A", "A", "A"]
+
+"""Submarines - Submarinos"""
+subma = ["S", "S", "S"]
+
+"""Destroyer - Destructor"""
+dest = ["D", "D"]
+
+"""Frigate - Fragata"""
+fri = ["F"]
